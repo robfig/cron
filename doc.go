b024fc5ea0e34bc3f83d9941c8d60b0622bfaca4
Rob Figueiredo <robfig@gmail.com>
2014-01-18 20:50:47 -0500

    Fix usage example
    Fixes #11

--- doc.go
+++ doc.go
@@ -7,7 +7,7 @@ Callers may register Funcs to be invoked on a given schedule.  Cron will run
 them in their own goroutines.
 
 	c := cron.New()
-	c.AddFunc("0 5 * * * *",  func() { fmt.Println("Every 5 minutes") })
+	c.AddFunc("0 30 * * * *", func() { fmt.Println("Every hour on the half hour") })
 	c.AddFunc("@hourly",      func() { fmt.Println("Every hour") })
 	c.AddFunc("@every 1h30m", func() { fmt.Println("Every hour thirty") })
 	c.Start()
