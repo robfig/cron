1e44e15c1848ef4d9951a006ce691ae87a4ab830
chenyunfei <chenyunfeihappy@gmail.com>
2016-03-30 11:49:33 +0800

    change the comments

--- cron.go
+++ cron.go
@@ -121,7 +121,7 @@ func (c *Cron) Entries() []*Entry {
 	return c.entrySnapshot()
 }
 
-// Start the cron scheduler in its own go-routine.
+// Start the cron scheduler in its own go-routine if it is not running, otherwize it does nothing.
 func (c *Cron) Start() {
 	if c.running {
 		return
