2f08104c458029e57e1bdc4c9426845de257de71
Rob Figueiredo <robfig@gmail.com>
2018-05-05 15:24:00 -0500

    Update doc.go to clarify @weekly

--- doc.go
+++ doc.go
@@ -78,7 +78,7 @@ You may use one of several pre-defined schedules in place of a cron expression.
 	-----                  | -----------                                | -------------
 	@yearly (or @annually) | Run once a year, midnight, Jan. 1st        | 0 0 0 1 1 *
 	@monthly               | Run once a month, midnight, first of month | 0 0 0 1 * *
-	@weekly                | Run once a week, midnight on Sunday        | 0 0 0 * * 0
+	@weekly                | Run once a week, midnight between Sat/Sun  | 0 0 0 * * 0
 	@daily (or @midnight)  | Run once a day, midnight                   | 0 0 0 * * *
 	@hourly                | Run once an hour, beginning of hour        | 0 0 * * * *
 
