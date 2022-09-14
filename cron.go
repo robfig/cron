539f1191c4a3147436608cb0e40da9566cffb36a
Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
2022-09-14 19:49:27 +0530

    Added ability to provide starts times for fixed rate schedules
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- cron.go
+++ cron.go
@@ -111,17 +111,17 @@ func (s byTime) Less(i, j int) bool {
 //
 // Available Settings
 //
-//   Time Zone
-//     Description: The time zone in which schedules are interpreted
-//     Default:     time.Local
+//	Time Zone
+//	  Description: The time zone in which schedules are interpreted
+//	  Default:     time.Local
 //
-//   Parser
-//     Description: Parser converts cron spec strings into cron.Schedules.
-//     Default:     Accepts this spec: https://en.wikipedia.org/wiki/Cron
+//	Parser
+//	  Description: Parser converts cron spec strings into cron.Schedules.
+//	  Default:     Accepts this spec: https://en.wikipedia.org/wiki/Cron
 //
-//   Chain
-//     Description: Wrap submitted jobs to customize behavior.
-//     Default:     A chain that recovers panics and logs them to stderr.
+//	Chain
+//	  Description: Wrap submitted jobs to customize behavior.
+//	  Default:     A chain that recovers panics and logs them to stderr.
 //
 // See "cron.With*" to modify the default behavior.
 func New(opts ...Option) *Cron {
@@ -187,7 +187,7 @@ func (c *Cron) AddTimedJob(spec string, cmd TimedJob) (EntryID, error) {
 	if err != nil {
 		return 0, err
 	}
-	return c.ScheduleTimedJob(schedule, cmd), nil
+	return c.ScheduleTimedJob(schedule, cmd, time.Time{}), nil
 }
 
 // Schedule adds a Job to the Cron to be run on the given schedule.
@@ -212,7 +212,7 @@ func (c *Cron) Schedule(schedule Schedule, cmd Job) EntryID {
 
 // ScheduleTimedJob adds a Job to the Cron to be run on the given schedule.
 // The job is wrapped with the configured Chain.
-func (c *Cron) ScheduleTimedJob(schedule Schedule, cmd TimedJob) EntryID {
+func (c *Cron) ScheduleTimedJob(schedule Schedule, cmd TimedJob, startTime time.Time) EntryID {
 	c.runningMu.Lock()
 	defer c.runningMu.Unlock()
 	c.nextID++
@@ -221,6 +221,7 @@ func (c *Cron) ScheduleTimedJob(schedule Schedule, cmd TimedJob) EntryID {
 		Schedule:        schedule,
 		WrappedTimedJob: c.timedJobchain.Then(cmd),
 		TimedJob:        cmd,
+		Next:            startTime,
 	}
 	if !c.running {
 		c.entries = append(c.entries, entry)
@@ -299,8 +300,10 @@ func (c *Cron) run() {
 	// Figure out the next activation times for each entry.
 	now := c.now()
 	for _, entry := range c.entries {
-		entry.Next = entry.Schedule.Next(now)
-		c.logger.Info("schedule", "now", now, "entry", entry.ID, "next", entry.Next)
+		if entry.Next.IsZero() {
+			entry.Next = entry.Schedule.Next(now)
+			c.logger.Info("schedule", "now", now, "entry", entry.ID, "next", entry.Next)
+		}
 	}
 
 	for {
@@ -333,14 +336,21 @@ func (c *Cron) run() {
 						c.startJob(e.WrappedJob)
 					}
 					e.Prev = e.Next
-					e.Next = e.Schedule.Next(now)
+					if e.Next.IsZero() {
+						e.Next = e.Schedule.Next(now)
+					} else {
+						e.Next = e.Schedule.Next(e.Next)
+					}
+
 					c.logger.Info("run", "now", now, "entry", e.ID, "next", e.Next)
 				}
 
 			case newEntry := <-c.add:
 				timer.Stop()
 				now = c.now()
-				newEntry.Next = newEntry.Schedule.Next(now)
+				if newEntry.Next.IsZero() {
+					newEntry.Next = newEntry.Schedule.Next(now)
+				}
 				c.entries = append(c.entries, newEntry)
 				c.logger.Info("added", "now", now, "entry", newEntry.ID, "next", newEntry.Next)
 
